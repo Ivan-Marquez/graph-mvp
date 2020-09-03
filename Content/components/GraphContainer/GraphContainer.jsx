5f69121f9a89354631d6e8f6dcc832fb074e5a63
Ivan Marquez <js.ivan.marquez@gmail.com>
2020-09-02 20:04:24 -0400

    IMP: adds handlers for GraphContainer component

--- Content/components/GraphContainer/GraphContainer.jsx
+++ Content/components/GraphContainer/GraphContainer.jsx
@@ -1,26 +1,17 @@
-import { Fragment } from "react";
-import styled from "styled-components";
+import React, { Fragment } from "react";
+import { injectGlobal } from "emotion";
+import { useGraphContainer } from "./lib";
+import Breadcrumbs from "../Breadcrumbs/Breadcrumbs.jsx";
 import Graph from "../Graph/Graph.jsx";
-import data from "./mockData";
 
-function GraphContainer() {
-  return (
-    <Fragment>
-      <div className="subheader"></div>
-      <div className="service-traffic-map">
-        <Graph traffic={data} />
-      </div>
-    </Fragment>
-  );
-}
-
-const StyledGraphContainer = styled(GraphContainer)`
+injectGlobal`
   .selected-formatted-date-time,
   .reset-layout-link {
     display: inline-block;
     line-height: 24px;
     color: var(--colorNormalDimmed);
   }
+
   .reset-layout-link {
     padding-left: 15px;
     -webkit-user-select: none;
@@ -30,4 +21,86 @@ const StyledGraphContainer = styled(GraphContainer)`
   }
 `;
 
-export default StyledGraphContainer;
+function GraphContainer(props) {
+  const state = useGraphContainer(props);
+
+  const { trafficData } = state;
+  const breadcrumbsRoot = _.get(trafficData, "name", "root");
+  const showBreadcrumbs = trafficData && trafficData.nodes && trafficData.nodes.length > 0;
+
+  function navigationCallback(newNavigationState) {
+    state.setCurrentView(newNavigationState);
+  }
+
+  function viewChanged({ graph, view }) {
+    state.setSearchTerm("");
+    state.setCurrentView(view);
+    state.setMatches({ total: -1, visible: -1 });
+    state.setCurrentGraph(graph);
+    graph.type !== "global" ? state.setRedirectFrom(graph.parentGraph.name) : state.setRedirectFrom("");
+  }
+
+  function objectHighlighted(highlightedObject) {
+    state.setHighlightedObject(highlightedObject);
+    state.setObjectToHighlight(highlightedObject ? highlightedObject.getName() : undefined);
+    state.setSearchTerm("");
+    state.setMatches({ total: -1, visible: -1 });
+    state.setRedirectFrom(undefined);
+  }
+
+  function zoomCallback() {
+    let currentView = state.currentView.slice();
+
+    if (state.highlightedObject) {
+      currentView.push(state.highlightedObject.name);
+      state.setObjectToHighlight(undefined);
+    } else if (state.currentView.length > 0) {
+      const nodeName = currentView.pop();
+
+      state.setObjectToHighlight(nodeName);
+    }
+
+    this.setState(newState);
+  }
+
+  function nodeClicked(node) {
+    if (state.currentView.length === 1) {
+      // highlight node
+      setObjectToHighlight({ objectToHighlight: node.getName() });
+    } else if (state.currentView.length === 2) {
+      // detailed view of node
+      setCurrentView([state.currentView[0], node.getName()]);
+    }
+  }
+
+  const { displayOptions } = state;
+
+  return (
+    <Fragment>
+      <div className="subheader">
+        {showBreadcrumbs && (
+          <Breadcrumbs
+            rootTitle={breadcrumbsRoot}
+            navigationStack={state.currentView}
+            navigationCallback={navigationCallback}
+          />
+        )}
+      </div>
+      <div className="service-traffic-map">
+        <Graph
+          traffic={trafficData}
+          view={state.currentView}
+          viewChanged={viewChanged}
+          allowDraggingOfNodes={displayOptions.allowDraggingOfNodes}
+          objectToHighlight={state.objectToHighlight}
+          objectHighlighted={objectHighlighted}
+          showLabels={displayOptions.showLabels}
+          definitions={state.definitions}
+          match={state.searchTerm}
+        />
+      </div>
+    </Fragment>
+  );
+}
+
+export default GraphContainer;
